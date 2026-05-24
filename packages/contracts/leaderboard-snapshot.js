1fea1798ea88588f17418796240bc89b5f241e6a
eheroboy <eherochaos@hotmail.com>
2026-05-25 00:55:24 +0800

    Add leaderboard API and CI

---
+++ packages/contracts/leaderboard-snapshot.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const LEADERBOARD_SNAPSHOT_ENDPOINT = "/api/leaderboard-snapshot";
+
+const LEADERBOARD_SNAPSHOT_ROOT_FIELDS = Object.freeze([
+  "metadata",
+  "home",
+  "tierRows",
+]);
+
+const LEADERBOARD_SNAPSHOT_METADATA_FIELDS = Object.freeze([
+  "sourceRunId",
+  "sourceKind",
+  "targetVersion",
+  "dateFrom",
+  "dateTo",
+  "updatedAt",
+  "sampleSize",
+  "excludedInvalidDeckRows",
+  "excludedInvalidDeckSampleSize",
+]);
+
+const LEADERBOARD_SNAPSHOT_HOME_FIELDS = Object.freeze([
+  "factionShare",
+  "representativeDecks",
+  "featuredCards",
+  "summary",
+]);
+
+module.exports = {
+  LEADERBOARD_SNAPSHOT_ENDPOINT,
+  LEADERBOARD_SNAPSHOT_HOME_FIELDS,
+  LEADERBOARD_SNAPSHOT_METADATA_FIELDS,
+  LEADERBOARD_SNAPSHOT_ROOT_FIELDS,
+};
